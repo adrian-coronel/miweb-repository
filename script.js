325311ef679a7f86f3f05a79bdac75a19a0ec3ba
Adrian Coronel Mendoza <adrianonel78@gmail.com>
2023-01-14 03:21:25 -0500

    Se realizó un html que saluda gracias a JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Hola soy un mensaje de prueba
-// Otro mensaje de prueba
 
-function saludar(name){
+function saludo(name){
     console.log(`Hola ${name}`);
 }
 
-saludar("Adrian");
-saludar("Junior");
-saludar("Leonardo");
+function saludar(){
+    var inputName = document.getElementById('input-name').value;
+    saludo(inputName);
+}
